3a73bd9d2a88cbc1bb43cef6214d5b0ac46500fa
Abdul Matin Esmat <matin@final.edu.tr.com>
2025-07-10 13:57:10 +0300

    Describe your update here

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,14 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/ssr';
 
-const supabase = createBrowserClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    global: {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+  }
 );
 
-export { supabase };
